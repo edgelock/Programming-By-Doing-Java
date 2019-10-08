664c64d60beffc499b8a900c90f358a558a59a54
edgelock <44417754+edgelock@users.noreply.github.com>
2019-10-07 23:53:50 -0500

    Add files via upload

---
+++ CountingByHalves.java
@@ -0,0 +1,15 @@
+package compsciwork;
+
+public class CountingByHalves {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double n;
+		for(n= -10.0 ; n <=10.0; n=n+0.5)
+		{
+			System.out.println(n);
+		}
+	}
+
+}
