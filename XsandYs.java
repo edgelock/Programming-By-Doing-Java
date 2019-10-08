067ff81db5ecc26e534658ec8a94ff248b07267c
edgelock <44417754+edgelock@users.noreply.github.com>
2019-10-08 00:00:25 -0500

    Add files via upload

---
+++ XsandYs.java
@@ -0,0 +1,20 @@
+package compsciwork;
+
+public class XsandYs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double x;
+		double y;
+		
+		for(x = -10.0; x<=10.0; x=x+0.5)
+		{
+			y = x*x;
+			
+			System.out.println(x+"	"+y);
+			
+		}
+	}
+
+}
